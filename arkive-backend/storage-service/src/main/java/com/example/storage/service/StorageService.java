3b5545c9ed2410cbcbd73a17cd37914cf98c3bad
muohannedxd <mohanned.kadache@ensia.edu.dz>
2025-05-08 20:13:25 +0100

    added storage-service

--- arkive-backend/storage-service/src/main/java/com/example/storage/service/StorageService.java
+++ arkive-backend/storage-service/src/main/java/com/example/storage/service/StorageService.java
@@ -3,7 +3,6 @@ package com.example.storage.service;
 import io.minio.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
