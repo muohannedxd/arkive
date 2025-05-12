e8ae06343c98029434cb61b0785722f0e9294c2b
muohannedxd <mohanned.kadache@ensia.edu.dz>
2025-05-12 20:49:33 +0100

    folders done

--- arkive-dms/src/views/dashboard/documents/index.tsx
+++ arkive-dms/src/views/dashboard/documents/index.tsx
@@ -189,7 +189,7 @@ export default function Documents() {
                   </Button>
                 </div>
 
-                <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5">
                   {documentsData.map((doc) => (
                     <FileCard
                       key={doc.id}
